4f09ec8be4406885021d467360111767ebae00be
TheSaviourEking <saviour.eking@outlook.com>
2023-10-03 13:37:19 +0100

    add solution to 3-convertToBase16.js

--- 1-base-conversions/3-convertToBase16.js
+++ 1-base-conversions/3-convertToBase16.js
@@ -4,26 +4,40 @@
 
 const convertToBase16 = element => {
   // Your code here
+  let map = { 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f' };
+  let result = [];
+  while (element > 0) {
+    let remainder = element % 16;
+    if (remainder in map) {
+      remainder = map[remainder];
+    }
+    element = Math.floor(element / 16);
+    result.push(remainder);
+  }
+
+  return "0x" + result.reverse().join('');
 };
 
 /******************************************************************************/
 
-console.log(convertToBase16(4)); // 0x4
-console.log(convertToBase16(65)); // 0x41
-console.log(convertToBase16(256)); // 0x100
-console.log(convertToBase16(123)); // 0x7b
-console.log(convertToBase16(1000)); // 0x3e8
+// console.log(convertToBase16(4)); // 0x4
+// console.log(convertToBase16(65)); // 0x41
+// console.log(convertToBase16(256)); // 0x100
+// console.log(convertToBase16(123)); // 0x7b
+// console.log(convertToBase16(1000)); // 0x3e8
 
-console.log('––––––');
+// console.log('––––––');
 
-console.log(convertToBase16('0b1100')); // 0xc
-console.log(convertToBase16('0b0101')); // 0x5
-console.log(convertToBase16('0b1000')); // 0x8
-console.log(convertToBase16('0b0111')); // 0x7
+// console.log(convertToBase16('0b1100')); // 0xc
+// console.log(convertToBase16('0b0101')); // 0x5
+// console.log(convertToBase16('0b1000')); // 0x8
+// console.log(convertToBase16('0b0111')); // 0x7
 
-console.log('––––––');
+// console.log('––––––');
 
-console.log(convertToBase16('0b10100101')); // 0xa5
-console.log(convertToBase16('0b11111111')); // 0xff
-console.log(convertToBase16('0b01010101')); // 0x55
-console.log(convertToBase16('0b00110011')); // 0x33
+// console.log(convertToBase16('0b10100101')); // 0xa5
+// console.log(convertToBase16('0b11111111')); // 0xff
+// console.log(convertToBase16('0b01010101')); // 0x55
+// console.log(convertToBase16('0b00110011')); // 0x33
+
+module.exports = convertToBase16;
